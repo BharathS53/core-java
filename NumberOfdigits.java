e67a282b41811bca72236e999ed2f1569548edad
Bharath s <bharaths9743@gmail.com>
2025-08-14 11:36:35 +0530

    13/08/2025

---
+++ NumberOfdigits.java
@@ -0,0 +1,10 @@
+public class NumberOfdigits {
+    public static void main(String[] args){
+        int arr[]={1,2,3,4};
+        int count=0;
+        for(int i=0;i<arr.length;i++){
+            count=count+1;
+        }
+        System.out.println(count);
+    }
+}
