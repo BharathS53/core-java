9549e2cf1db9573db36b3ac68496f530fabc8de7
Bharath s <bharaths9743@gmail.com>
2025-08-21 10:24:19 +0530

    Add files via upload
    20/8/2025

---
+++ Horse.java
@@ -0,0 +1,22 @@
+package com.xorkz.inheritance;
+
+public class Horse extends Animal {
+    private String color;
+    private int speed;
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+}
